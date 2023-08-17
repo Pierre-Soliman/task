21416e056791ec7cfa95d32bce84206ecc77aacd
Pierre-Soliman <premon320@gmail.com>
2023-08-17 14:18:37 +0300

    sixth commit

--- lib/Widgets/category.dart
+++ lib/Widgets/category.dart
@@ -5,49 +5,37 @@ class categeory extends StatelessWidget {
     super.key,
     required this.image,
     required this.txt,
- 
- 
   });
   final String image;
   final String txt;
-
-
-
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      // onTap: press,
       child: Stack(
-        children: [         
-                Container(
-                  height: 95,
-                  margin: EdgeInsets.all(2),
-                  child: Image.asset(
-                    image,
-                    fit: BoxFit.cover,
-                    height: 100,
-                    
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-               Positioned(
-                bottom: 15,
-                left: 12,
-                // top: 2,
-                 child: Container(
-                    child: Text(
-                      txt,
-                      style: TextStyle(color: Colors.white, fontSize: 12),
-                    ),
-                  ),
-               ),
-              ],
-            
-          
-        
-      
+        children: [
+          Container(
+            height: 95,
+            margin: EdgeInsets.all(2),
+            child: Image.asset(
+              image,
+              fit: BoxFit.cover,
+              height: 100,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          Positioned(
+            bottom: 15,
+            left: 12,
+            child: Container(
+              child: Text(
+                txt,
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
